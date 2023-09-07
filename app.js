a74dee6853825246983f7e484288a2b41ccff2bb
Muhammad Bilal <bilalshahidvayani@gmail.com>
2023-09-07 21:46:55 +0500

    two file add

---
+++ app.js
@@ -0,0 +1,89 @@
+// Chapter 1 (Alerts)
+// q1
+alert("first name");
+alert("Last Name");
+alert("Email");
+alert("Phone Number");
+alert("Password");
+// q2
+alert("You're learning JavaScript!");
+// q3
+alert("You're learning JavaScript!");
+// Chapter 2 (Variables for string)
+// q1
+var a 
+// q2
+var b=("144")
+// q3
+var teamname=("Pakistan");
+alert("Pakistan");
+// q4
+var bestMan = "Charlie"
+man="jeo";
+console.log(bestMan+man);
+// Chapter 3 (Variables for numbers)
+// q1
+var caseQty ;
+// q2
+var caseQty = 144;
+console.log(caseQty);
+// q3
+var num = "9";
+console.log(num)
+// q4
+var dec;
+sum=2
+console.log(sum)
+// q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+// q6
+var num1="10"
+num="20";
+console.log(num1+num);
+// Chapter 4 (Variable names Legal and Illegal)
+// q1
+var productcost = 3.45;
+console.log(productcost);
+// q2
+var Nameofband;
+// q3
+var num1=200;
+console.log(num1)
+// q4
+var firstname=bilal
+lastname=vayani
+console.log(firstname+lastname);
+// q5
+// legal
+var num1
+var firstname
+var lastname
+var Phonenumber
+var Password
+// illegal
+var a
+var c
+var b
+var d
+var e
+// Chapter 5 (Math Expression I)
+// q1
+var q1 ="Modulus Operator"
+// q2
+var num5 = 20 % 6;
+console.log(num5)
+// q3
+var largeNum = 1000*2000;
+console.log(largeNum);
+// q4
+var num5 = 20 - 6;
+console.log(num5)
+// q5
+var num5 = 20 / 6;
+console.log(num5)
+// q6
+var num5 = 20 * 6;
+alert(num5)
